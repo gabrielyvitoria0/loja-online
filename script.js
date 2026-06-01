85ea20d686d10b41460635b57131851e4c0e4070
GABRIELY VITORIA FELIX DE SOUZA <gabrielyv896@gmail.com>
2026-06-01 07:52:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-function comprar(produto){
-    alert(
-        "você comprou: " + produto
-    )
+async function comprar(produto, preco){
+    const respota = await fetch("/checkout", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            produto,
+            preco
+        })
+
+    })
+
+    const dadaos = await respota.json()
+    const stripe = stripe("pk_test_51TZrXnRua0kTWEDBDwKYK10S8gGOfMU96ZTUelQcudrX9tuEz7buuJw9ac0XiIKCtB3Us829iZyGToWymjNQNv5T00LPsV5TYj")
+    sessionid: dados.id
 }
